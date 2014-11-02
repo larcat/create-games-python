651e07ad291a3df9da475bd425f76ce535068fc8
larcat <larcatdm@gmail.com>
2014-11-02 17:32:13 -0600

    Delete Chap 1 Calculator.py

--- Chapter 1/Chap 1 Calculator.py
+++
@@ -1,6 +0,0 @@
-faren_temp = float(input("What temp in F do you want to convert? "))
-
-celcius_temp = (faren_temp - 32.0) * (5.0/9.0)
-
-print("When the temp in Farenheit is {:.2f}: ".format(faren_temp))
-print("The temp in Celcius is: {:.2f}".format(celcius_temp))
