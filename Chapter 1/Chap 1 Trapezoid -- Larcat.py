b0717dfae495e9b0a3ea7e03b22ff9bf8e80a8d9
larcat <larcatdm@gmail.com>
2014-11-02 18:19:04 -0600

    Trapezoid caclualtor added.

---
+++ Chapter 1/Chap 1 Trapezoid -- Larcat.py
@@ -0,0 +1,8 @@
+topside = float(input("Enter the length of the top side of the trapezoid: "))
+bottomside = float(input("Enter the length of the bottom side of the trapezoid: "))
+height = float(input("Enter the height of the trapezoid: "))
+
+area = 0.5 * (topside + bottomside) * height
+
+print("The area of your trapezoid is: {:.2f} units squared".format(area))
+
